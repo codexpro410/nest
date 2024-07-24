34092e79659cdce8f697c4a297f4dcf75838530c
islamabozeed <islammahmoud13@gmail.com>
2024-07-24 19:26:53 +0300

    fix

--- src/apps/front-office/home/pages/HomePage/Slider.tsx
+++ src/apps/front-office/home/pages/HomePage/Slider.tsx
@@ -38,7 +38,7 @@ export default function Slider() {
           <SwiperSlide key={slide.id}>
             <div className="relative flex items-center justify-center p-6 rounded-3xl">
               <img src={slide.image} className="bg-cover bg-center bg-no-repeat object-cover rounded-3xl w-full h-full min-h-[350px]"/>
-              <section className="absolute top-10 left-8 sm:left-20 flex flex-col justify-center gap-2 items-start">
+              <section className="absolute top-1/2 bottom-1/2 left-8 sm:left-20 flex flex-col justify-center gap-2 items-start">
                 <p className="pt-[20px] font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-[72px]">{slide.text1}</p>
                 <p className="pt-[20px] font-semibold text-2xl sm:text-3xl md:text-4xl lg:text-[72px]">{slide.text2}</p>
                 <p className="pt-[50px]  lg:text-[30px]">{slide.text3}</p>
@@ -55,11 +55,9 @@ export default function Slider() {
           </SwiperSlide>
         ))}
         
-        {/* Custom pagination */}
         <div className="swiper-pagination absolute !bottom-5 left-1/2  transform -translate-x-1/2 z-10"></div>
       </Swiper>
 
-      {/* Custom navigation buttons */}
       <div className="swiper-button-prev absolute top-1/2 !left-7 z-10 w-10 h-10 rounded-full bg-white opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 hover:bg-green-500"></div>
       <div className="swiper-button-next absolute top-1/2 !right-7 z-10 w-10 h-10 rounded-full bg-white opacity-0 transition-all duration-300 ease-in-out group-hover:opacity-100 hover:bg-green-500"></div>
 
